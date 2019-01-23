09f1472392a71f8413cd34651d7ae9574e31c473
narendramanchala <narendra.manchala@nextsteps.ai>
2019-01-23 12:36:39 +0530

    Updates

---
+++ precache-manifest.c397a2ffffe962fddfd7460ced5263cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "a42c4387165d10cfa16b",
+    "url": "/website/static/js/main.a42c4387.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/website/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a42c4387165d10cfa16b",
+    "url": "/website/static/css/main.d1bf4177.chunk.css"
+  },
+  {
+    "revision": "eb47e628d1d03c46a3d520e44bbbb715",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/narendramanchala/precache-manifest.debcbb5caf7824b106040777082c2bb4.js"
+  "/website/precache-manifest.c397a2ffffe962fddfd7460ced5263cf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/narendramanchala/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
